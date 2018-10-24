ee544de18f328794ddd11717e1498903fc606df1
Mateo Codesido <mateo_2992@hotmail.com>
2018-10-24 09:38:33 +0200

    Introducido el Switch

--- index.js
+++ index.js
@@ -8,4 +8,36 @@ function onClick(evento) {
   if (evento.target.tagName === "BUTTON") {
     result.innerText = boton.innerText;
   }
+  let characters = evento.target.innerText;
+  switch (characters) {
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+    case "0":
+      result.innerText = evento.target.innerText;
+      break;
+    case "C":
+      result.innerText = "0";
+      break;
+    case "÷":
+    case "×":
+    case "-":
+    case "+":
+      result.innerText = "";
+      break;
+    case "←":
+      result.innerText = "";
+      break;
+    case "=":
+      result.innerText = "";
+      break;
+    default:
+      console.log("Aquí hay un cable suelto amigo...");
+  }
 }
